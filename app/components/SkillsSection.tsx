8ea9cbc70efc011749c52ff622bcd7d0e164016c
Vincent Toor-Azorín <vincenttoorazorin@mac.home>
2025-10-31 18:21:22 +0000

    Made skills language-sensitive

--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -8,18 +8,18 @@ interface SkillsSectionProps {
 export default function SkillsSection({ language }: SkillsSectionProps) {
 	return (
 		<div>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[0]} skills={programming_languages}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[1]} skills={database_management}/>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[2]} skills={cloud_services}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[3]} skills={infrastructure_as_code}/>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[4]} skills={data_pipelines}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[5]} skills={APIs_and_web_servers}/>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[6]} skills={version_control}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[7]} skills={testing}/>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[8]} skills={data_analysis}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[9]} skills={AI_frameworks}/>
-			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[10]} skills={website_development}/>
-			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[11]} skills={methodology}/>
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[0]} skills={skills.programming_languages[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[1]} skills={skills.database_management[language as Language]} />
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[2]} skills={skills.cloud_services[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[3]} skills={skills.infrastructure_as_code[language as Language]} />
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[4]} skills={skills.data_pipelines[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[5]} skills={skills.APIs_and_web_servers[language as Language]} />
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[6]} skills={skills.version_control[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[7]} skills={skills.testing[language as Language]} />
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[8]} skills={skills.data_analysis[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[9]} skills={skills.AI_frameworks[language as Language]} />
+			<SkillsComponent mode="dark" text={webPageText[language as Language].SkillsSection.skill_categories[10]} skills={skills.website_development[language as Language]} />
+			<SkillsComponent mode="light" text={webPageText[language as Language].SkillsSection.skill_categories[11]} skills={skills.methodology[language as Language]} />
 		</div>
 	);
 }
@@ -59,15 +59,77 @@ function SkillsComponent({ mode, text, skills }: SkillsComponentProps) {
 	)
 }
 
-const programming_languages = ['Python', 'HTML/CSS', 'JavaScript/TypeScript', 'MATLAB', 'Java', 'C']
-const database_management = ['Excel', 'SQL', 'PostgreSQL', 'MongoDB', 'Database Modeling', 'Normalisation', 'Star Schema', 'Warehouse Design']
-const cloud_services = ['AWS', 'Microsoft Azure', 'Google Cloud']
-const infrastructure_as_code = ['Terraform', 'YAML']
-const data_pipelines = ['ETL pipelines']
-const APIs_and_web_servers = ['FastAPI', 'Django', 'Flask', 'http.server', 'Python requests', 'Express.js', 'CORS', 'Axios', 'Client-Server Architecture']
-const version_control = ['Git', 'GitHub']
-const testing = ['Test-Driven Development', 'Pytest']
-const data_analysis = ['Numpy', 'Pandas', 'Matplotlib', 'SciPy']
-const AI_frameworks = ['PyTorch', 'TensorFlow', 'LangChain', 'LangGraph', 'LlamaIndex', 'Scikit-Learn']
-const website_development = ['Node.js', 'Next.js', 'React.js', 'TailwindCSS']
-const methodology = ['Agile', 'Trello']
+const skills = {
+  programming_languages: {
+    EN: ['Python', 'HTML/CSS', 'JavaScript/TypeScript', 'MATLAB', 'Java', 'C'],
+    FR: ['Python', 'HTML/CSS', 'JavaScript/TypeScript', 'MATLAB', 'Java', 'C'],
+    ES: ['Python', 'HTML/CSS', 'JavaScript/TypeScript', 'MATLAB', 'Java', 'C'],
+    DE: ['Python', 'HTML/CSS', 'JavaScript/TypeScript', 'MATLAB', 'Java', 'C']
+  },
+  database_management: {
+    EN: ['Excel', 'SQL', 'PostgreSQL', 'MongoDB', 'Database Modeling', 'Normalisation', 'Star Schema', 'Warehouse Design'],
+    FR: ['Excel', 'SQL', 'PostgreSQL', 'MongoDB', 'Modélisation de base de données', 'Normalisation', 'Schéma en étoile', 'Conception d\'entrepôt de données'],
+    ES: ['Excel', 'SQL', 'PostgreSQL', 'MongoDB', 'Modelado de bases de datos', 'Normalización', 'Esquema de estrella', 'Diseño de almacén de datos'],
+    DE: ['Excel', 'SQL', 'PostgreSQL', 'MongoDB', 'Datenbankmodellierung', 'Normalisierung', 'Sternschema', 'Data-Warehouse-Design']
+  },
+  cloud_services: {
+    EN: ['AWS', 'Microsoft Azure', 'Google Cloud'],
+    FR: ['AWS', 'Microsoft Azure', 'Google Cloud'],
+    ES: ['AWS', 'Microsoft Azure', 'Google Cloud'],
+    DE: ['AWS', 'Microsoft Azure', 'Google Cloud']
+  },
+  infrastructure_as_code: {
+    EN: ['Terraform', 'YAML'],
+    FR: ['Terraform', 'YAML'],
+    ES: ['Terraform', 'YAML'],
+    DE: ['Terraform', 'YAML']
+  },
+  data_pipelines: {
+    EN: ['ETL pipelines'],
+    FR: ['Pipelines ETL'],
+    ES: ['Pipelines ETL'],
+    DE: ['ETL-Pipelines']
+  },
+  APIs_and_web_servers: {
+    EN: ['FastAPI', 'Django', 'Flask', 'http.server', 'Python requests', 'Express.js', 'CORS', 'Axios', 'Client-Server Architecture'],
+    FR: ['FastAPI', 'Django', 'Flask', 'http.server', 'Requêtes Python', 'Express.js', 'CORS', 'Axios', 'Architecture Client-Serveur'],
+    ES: ['FastAPI', 'Django', 'Flask', 'http.server', 'Peticiones Python', 'Express.js', 'CORS', 'Axios', 'Arquitectura Cliente-Servidor'],
+    DE: ['FastAPI', 'Django', 'Flask', 'http.server', 'Python Requests', 'Express.js', 'CORS', 'Axios', 'Client-Server-Architektur']
+  },
+  version_control: {
+    EN: ['Git', 'GitHub'],
+    FR: ['Git', 'GitHub'],
+    ES: ['Git', 'GitHub'],
+    DE: ['Git', 'GitHub']
+  },
+  testing: {
+    EN: ['Test-Driven Development', 'Pytest'],
+    FR: ['Développement piloté par les tests', 'Pytest'],
+    ES: ['Desarrollo guiado por pruebas', 'Pytest'],
+    DE: ['Testgetriebene Entwicklung', 'Pytest']
+  },
+  data_analysis: {
+    EN: ['Numpy', 'Pandas', 'Matplotlib', 'SciPy'],
+    FR: ['Numpy', 'Pandas', 'Matplotlib', 'SciPy'],
+    ES: ['Numpy', 'Pandas', 'Matplotlib', 'SciPy'],
+    DE: ['Numpy', 'Pandas', 'Matplotlib', 'SciPy']
+  },
+  AI_frameworks: {
+    EN: ['PyTorch', 'TensorFlow', 'LangChain', 'LangGraph', 'LlamaIndex', 'Scikit-Learn'],
+    FR: ['PyTorch', 'TensorFlow', 'LangChain', 'LangGraph', 'LlamaIndex', 'Scikit-Learn'],
+    ES: ['PyTorch', 'TensorFlow', 'LangChain', 'LangGraph', 'LlamaIndex', 'Scikit-Learn'],
+    DE: ['PyTorch', 'TensorFlow', 'LangChain', 'LangGraph', 'LlamaIndex', 'Scikit-Learn']
+  },
+  website_development: {
+    EN: ['Node.js', 'Next.js', 'React.js', 'TailwindCSS'],
+    FR: ['Node.js', 'Next.js', 'React.js', 'TailwindCSS'],
+    ES: ['Node.js', 'Next.js', 'React.js', 'TailwindCSS'],
+    DE: ['Node.js', 'Next.js', 'React.js', 'TailwindCSS']
+  },
+  methodology: {
+    EN: ['Agile', 'Trello'],
+    FR: ['Agile', 'Trello'],
+    ES: ['Ágil', 'Trello'],
+    DE: ['Agile', 'Trello']
+  }
+}
